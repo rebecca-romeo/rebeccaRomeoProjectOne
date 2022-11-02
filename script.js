dfc91879d0c4e8ae4ae60c3c57062fa4788bb6de
rebecca-romeo <rebecca.romeo@ryerson.ca>
2022-11-02 08:40:40 -0400

    added commentation about js feature

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// This javascript feature is linked to the blog page. It it for the comment form. When the user submits their comment, an alert shows up to thank them for submitting their comment 
+
 const blogComment = document.querySelector('.blogForm');
 
 console.log(blogComment);
