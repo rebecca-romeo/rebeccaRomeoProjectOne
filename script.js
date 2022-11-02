ec12ee8aa5b357e686335a77b49ce3e75e1a6922
rebecca-romeo <rebecca.romeo@ryerson.ca>
2022-11-02 08:37:44 -0400

    added js feature on blog comment form

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const blogComment = document.querySelector('.blogForm');
+
+console.log(blogComment);
+
+blogComment.addEventListener('submit', function(){
+  alert('Thank you for leaving a comment.');
+});
+
+
+
+
